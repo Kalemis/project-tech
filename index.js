4f50936afb8f6007f1865514560caffccc08285b
Artemis <artemis-kalemis@hotmail.com>
2019-03-05 21:33:00 +0100

    Now really added the files

---
+++ index.js
@@ -0,0 +1,9 @@
+
+//import repeat from 'repeat-string';
+const repeat = require("repeat-string");
+const similarity = require("string-similarity");
+
+
+console.log(similarity.compareTwoStrings('healed', 'sealed'));
+
+console.log(repeat('A', 5));
